0db2e7355cb558f6b31cc87e575c1ecb7f1a1e8e
Mistire <mdwithgod@gmail.com>
2023-11-27 22:56:21 +0300

    Delete temprunnerfile

--- Week 2/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-   # elif choice == 2:
-    #   name_view = input("Enter the name of the Student you're searching for: ")
-    #   view_student(name_view)
-    # elif choice == 3:
-    #   list_students()
-    # elif choice == 4:
-    #   update_info()
-    # elif choice == 5:
-    #   delete_info()
