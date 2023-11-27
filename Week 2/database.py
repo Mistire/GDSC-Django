42e6029642bb6ec72ef581e31f8a0726f93391d0
Mistire <mdwithgod@gmail.com>
2023-11-27 18:34:24 +0300

    Create a menu function to display all actions

--- Week 2/database.py
+++ Week 2/database.py
@@ -1,11 +1,28 @@
 student_db = {}
 
-def add_student(name, id, age, grade):
+def menu():
+  print("Welcome to the Student Database")
+  while True:
+    print("\n1. Add a new student.")
+    print("2. Display all students.")
+    print("3. Search for a student by name.")
+    print("4. Update a student's information.")
+    print("5. Delete a student from the database.")
+    print("6. Quit.")
+
+    choice = int(input("Enter your choice: "))
+
+
+menu()
+
+
+# def add_student(name, id, age, grade):
   
-def view_student(name):
+# def view_student(name):
 
-def list_students():
+# def list_students():
 
-def update_info():
+# def update_info():
 
-def delete_info(name):
+# def delete_info(name):
+
---
+++ Week 2/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+def add_student(name, id, age, grade):
+  
+# def view_student(name):
+
+# def list_students():
+
+# def update_info():
+
+# def delete_info(name):
