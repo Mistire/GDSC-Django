ae76a3f563ed99ea9e055a477b24e3ede0d83734
Mistire <mdwithgod@gmail.com>
2023-11-27 21:30:13 +0300

    Create a display function to display all the students informations in table format

--- Week 2/database.py
+++ Week 2/database.py
@@ -12,10 +12,16 @@ def add_student():
   
 
 def view_student(name):
-  
-  print(student_db[name])
-# def list_students():
-#   name = input()
+  if name in student_db:
+    for key, value in student_db[name].items():
+      print(key, ":", value)
+  else:
+    print("No such student found.")
+def list_students():
+  print("{:<10} {:<15} {:<10} {:<10}".format('ID_NO', 'NAME', 'SEX', 'AGE'))
+  for student_info in student_db.values():
+    id_no, name, sex, age = student_info["ID_no"], student_info["Name"], student_info["Sex"], student_info["Age"], 
+    print("{:<10} {:<15} {:<10} {:<10}".format(id_no, name, sex, age))
 # def update_info():
 #   name = input()
 # def delete_info(name):
@@ -25,8 +31,8 @@ def menu():
   print("Welcome to the Student Database")
   while True:
     print("\n1. Add a new student.")
-    print("2. Display all students.")
-    print("3. Search for a student by name.")
+    print("2. Search for a student by name.")
+    print("3. Display all student in the Database.")
     print("4. Update a student's information.")
     print("5. Delete a student from the database.")
     print("6. Exit.")
@@ -38,8 +44,8 @@ def menu():
     elif choice == 2:
       name_view = input("Enter the name of the Student you're searching for: ")
       view_student(name_view)
-    # elif choice == 3:
-    #   list_students()
+    elif choice == 3:
+      list_students()
     # elif choice == 4:
     #   update_info()
     # elif choice == 5:
