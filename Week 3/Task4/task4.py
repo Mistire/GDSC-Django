f16726d95f2b4aa2aa026e48367787a6a877b242
Mistire <mdwithgod@gmail.com>
2023-12-03 18:45:51 +0300

    Do task 4

--- Week 3/Task4/task4.py
+++ Week 3/Task4/task4.py
@@ -1,2 +1,16 @@
+sum = 0
+three_mul = 0
+five_mul = 0
 for i in range(1, 51):
-  if
+  if i % 3 == 0:
+    print("Three")
+    three_mul += 1
+  elif i % 5 == 0:
+    print("Five")
+    five_mul += 1
+  else:
+    sum += i
+    
+print(f"The Total Sums of evens are: {sum}")
+print(f"Count of numbers replaced with 'Three': {three_mul}")
+print(f"Count of numbers replaced with 'Five': {five_mul}")
