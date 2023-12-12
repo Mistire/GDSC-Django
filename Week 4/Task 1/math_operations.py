a83d82b6b89e30f3a517687112f8c7eb07c18e18
Mistire <mdwithgod@gmail.com>
2023-12-12 15:21:13 +0300

    Create task 1 functions

--- Week 4/Task 1/math_operations.py
+++ Week 4/Task 1/math_operations.py
@@ -1,22 +1,32 @@
 def basic_operations(a,b):
   '''Perform basic arithmetic operations and Return results in a dictionary'''
-  add = a + b
-  sub = a - b
-  mult = a * b
-  div = a / b
+  try:
+    add = a + b
+    sub = a - b
+    mult = a * b
+    div = a / b
+    result = {'sum':add, 'substract':sub,'product':mult,'quotient':div}
+  except ZeroDivisionError:
+    return "Can't divide with zero"
+  except TypeError:
+    return "Invalid input"
+  else:
+    return result
 
-  return({'sum':add, 'substract':sub,'product':mult,'quotient':div})
 
 def power_operation(a,b,**kwargs):
   '''Calculate power operation. If 'modulo' is provided in kwargs, calculate modulo'''
+  try:
+    result = a ** b
 
-  result = a ** b
-
-  if 'modulo' in kwargs:
-    value = kwargs['modulo']
-    result %= value
-
-  return result
+    if 'modulo' in kwargs:
+      value = kwargs['modulo']
+      result %= value
+  except ZeroDivisionError:
+    return "Can't divide with zero"
+  except TypeError:
+    return "Invalid input"
+  else:
+    return result
 
 def apply_operations(operation_list):
-  
