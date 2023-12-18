f724d332393861b1a1b066e218b4647113d2385e
Mistire <mdwithgod@gmail.com>
2023-12-18 14:36:38 +0300

    Create all the classses for the project

--- Week 5/Exercise/library.py
+++ Week 5/Exercise/library.py
@@ -6,7 +6,7 @@ class Book:
         self.availability_status = True
 
     def display_book_details(self):
-        return f"Title: {self.title}\nAuthor: {self.author}\nISBN: {self.ISBN}"
+        return f"Title: {self.title}\nAuthor: {self.author}\nISBN: {self.ISBN}\nAvailability: {'Available' if self.availability_status else 'Not Available'}\n"
     
     def update_status(self,status):
         self.availability_status = status
@@ -18,14 +18,24 @@ class User:
         self.books_borrowed = []
 
     def display_user(self):
-        return f"User ID: {self.user_id}, Name: {self.name}, Books Borrowed: "
+        return f"User ID: {self.user_id}, Name: {self.name}, Books Borrowed: {', '.join(self.books_borrowed)}"
     
     def borrow_book(self, books_title):
-        self.books_borrowed.append(books_title)
+        if availability_status:
+            self.books_borrowed.append(books_title)
+            availability_status = False
+            print(f"'{books_title}' has been successfully been borrowed by {self.name}")
+        else:
+            print(f"'{books_title}' isn't available right now")
         return f"Books borrowed: {self.books_borrowed}"
         
     def return_book(self, books_title):
-        self.books_borrowed.remove(books_title)
+        if books_title in self.books_borrowed:    
+            self.books_borrowed.remove(books_title)
+            availability_status = True
+            print(f"'{books_title}' is been returned to the library by {self.name}")
+        else:
+            print(f"You haven't borrowed the book '{books_title}' from the library")
 
 class Library:
     def __init__(self, users, books):
@@ -34,9 +44,21 @@ class Library:
     
     def add_book(self, book):
         self.books.append(book)
+        print(f"Book '{book.title}' added to the library.")
+
+    def display_books(self):
+        print("Books is in the library: ")
+        for book in self.books:
+            book.display_book_details()
 
     def register_user(self, user):
         self.users.append(user)
+        print(f"User '{user.name}' registered in the library.")
+
+    def display_users(self):
+        print("Users Registered in the library: ")
+        for user in self.users:
+            user.display_user()
 
     def borrowed_books(self, books_title):
         self.books.append(books_title)
